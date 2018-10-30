f798c2b5c44d90fc826f3a373f20e3e406f978ac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-30 19:30:29 +0000

    Deploy flightwallet/kassa to github.com/flightwallet/kassa.git:gh-pages

--- webpack/production.config.js
+++
@@ -1,9 +0,0 @@
-const base = require('./base.config');
-const merge = require('webpack-merge');
-
-const productionConfig = {
-  mode: 'production',
-  devtool: 'source-map', // activate source maps, see https://webpack.github.io/docs/configuration.html#devtool alternative cheap-module-eval-source-map: has proper source maps in development
-};
-
-module.exports = merge(base, productionConfig);
